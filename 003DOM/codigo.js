4dd01dc9a1b358c6e2c59dd500d31bbac365a87c
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-14 09:43:00 -0500

    separacion html y JS

--- 003DOM/codigo.js
+++ 003DOM/codigo.js
@@ -1,2 +1,3 @@
-
-alert ("si funciona");
+var ruta = window.location.protocol;
+console.log(ruta);
+document.write("hola care-bola: El objeto 'windows' tiene un atributo 'location' y este tiene un atributo 'protocol' que es -> " + ruta);
