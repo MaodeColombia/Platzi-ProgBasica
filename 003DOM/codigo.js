ed32b15a1348051f76858ee6481bec804724b631
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-14 09:39:52 -0500

    mas código, introducio de manera desordenada

--- 003DOM/codigo.js
+++ 003DOM/codigo.js
@@ -1,4 +1,2 @@
-var ruta = window.location;
-console.log(ruta);
-// NOTE:console.log() es para poder conocer cómo está escrito un objeto por dentro
-document.write("estás aprendiendo cositas \n" + ruta.pathname)
+
+alert ("si funciona");
