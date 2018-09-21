0efc3dd731326d70a8ed0dd49caafd87bb5a2505
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-21 16:31:24 -0500

    HTML&JS: paso a paso de la creación de una linea

--- 004canvas/JS_dibujo.js
+++ 004canvas/JS_dibujo.js
@@ -2,3 +2,10 @@ var aux = document.getElementById("lienzo_html");
 var lienzo_JS = aux.getContext("2d");
 console.log(lienzo_JS);
 console.log(aux);
+// NOTE: lo cacarcteristico de las siguiente 6 lineas es que desdpues de -iniciar linea -ir al lugar inicial -desplazarce a lugar final y -terminar la linea es que (depues de hace todo el trayecto) es cuando se le da color
+lienzo_JS.beginPath();
+lienzo_JS.moveTo(100,100);
+lienzo_JS.lineTo(200,200);
+lienzo_JS.closePath();
+lienzo_JS.strokeStyle="blue";
+lienzo_JS.stroke();
