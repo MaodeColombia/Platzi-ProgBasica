cd38b22b9fb27f2fcfcf3c8c00ad2d5fffd6c2ae
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-26 05:38:27 -0500

    EXTRA: cambio de nombre de variables

--- 004canvas/JS_dibujo.js
+++ 004canvas/JS_dibujo.js
@@ -1,6 +1,6 @@
 // NOTE: variables para el canvas en javascript
-var aux = document.getElementById("lienzo_html");
-var lienzo_JS = aux.getContext("2d");
+var objeto_canvas = document.getElementById("lienzo_html");
+var lienzo_2D = objeto_canvas.getContext("2d");
 
 // NOTE: variables para el cuador de tecto y el boton
 var user_input_js = document.getElementById('user_input_html');
@@ -9,18 +9,15 @@ var boton_js = document.getElementById('boton_html');
 boton_js.addEventListener("click",dibujoPorClick);
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal){
-  lienzo_JS.beginPath();
-  lienzo_JS.moveTo(xinicial,yinicial);
-  lienzo_JS.lineTo(xfinal,yfinal);
-  lienzo_JS.closePath();
-  lienzo_JS.strokeStyle=color;
-  lienzo_JS.stroke();}
-function dibujoPorClick()
-{
-  var pixeles_canvas = 300; //los pixeles que hay en el pixeles_canvas
+  lienzo_2D.beginPath();
+  lienzo_2D.moveTo(xinicial,yinicial);
+  lienzo_2D.lineTo(xfinal,yfinal);
+  lienzo_2D.closePath();
+  lienzo_2D.strokeStyle=color;
+  lienzo_2D.stroke();}
+function dibujoPorClick(){
+  var pixeles_canvas = objeto_canvas.width; //los pixeles que hay en el pixeles_canvas
   var densidad = parseInt(user_input_js.value);//las lineas que quiero
   var dxdy = pixeles_canvas/densidad; //cantidad de pixeles que distancian una linea y la siguiente.
-
   for (aux1=0;dxdy*aux1<pixeles_canvas;aux1++){
-      dibujarLinea("grey",dxdy*aux1,0,300,dxdy*(aux1+1));}
-}
+      dibujarLinea("grey",dxdy*aux1,0,300,dxdy*(aux1+1));}}
