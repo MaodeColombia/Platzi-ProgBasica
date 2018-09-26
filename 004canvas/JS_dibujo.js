ebff2276eecd5aa15b51addcd3e608d9d21ccfd4
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-26 05:19:52 -0500

    HTML&JS: genera figura solo cuando "cuadro de texto" y se pincha boton

--- 004canvas/JS_dibujo.js
+++ 004canvas/JS_dibujo.js
@@ -1,16 +1,12 @@
+// NOTE: variables para el canvas en javascript
 var aux = document.getElementById("lienzo_html");
 var lienzo_JS = aux.getContext("2d");
+
+// NOTE: variables para el cuador de tecto y el boton
 var user_input_js = document.getElementById('user_input_html');
 var boton_js = document.getElementById('boton_html');
-boton_html.addEventListener("click",dibujoPorClick);
-console.log(user_input_html);
 
-var pixeles_canvas = 300; //los pixeles que hay en el pixeles_canvas
-var densidad = 50;//las lineas que quiero
-var dxdy = pixeles_canvas/densidad; //cantidad de pixeles que distancian una linea y la siguiente.
-
-for (aux1=0;dxdy*aux1<pixeles_canvas;aux1++){
-    dibujarLinea("grey",dxdy*aux1,0,300,dxdy*(aux1+1));}
+boton_js.addEventListener("click",dibujoPorClick);
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal){
   lienzo_JS.beginPath();
@@ -19,4 +15,12 @@ function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal){
   lienzo_JS.closePath();
   lienzo_JS.strokeStyle=color;
   lienzo_JS.stroke();}
-function dibujoPorClick(){alert(user_input_js.value);}
+function dibujoPorClick()
+{
+  var pixeles_canvas = 300; //los pixeles que hay en el pixeles_canvas
+  var densidad = parseInt(user_input_js.value);//las lineas que quiero
+  var dxdy = pixeles_canvas/densidad; //cantidad de pixeles que distancian una linea y la siguiente.
+
+  for (aux1=0;dxdy*aux1<pixeles_canvas;aux1++){
+      dibujarLinea("grey",dxdy*aux1,0,300,dxdy*(aux1+1));}
+}
