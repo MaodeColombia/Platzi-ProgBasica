53e9ba0f9820bd9a8fa079e72c2339ce4babfa92
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-29 18:52:53 -0500

    Creacion de objeto con los # de las fechas y uso del switch

--- 005canvasconflechas/figura_teclado.js
+++ 005canvasconflechas/figura_teclado.js
@@ -1,7 +1,23 @@
-var canvas_js = document.getElementById("canvas_html");
-var lienzo = canvas_js.getContext("2D");
- 
-document.addEventListener("keydown", eventoTeclado);
+var teclas ={LEFT:37,UP:38,RIGHT:39,DOWN:40};
+console.log(teclas);
+
+document.addEventListener("keyup", eventoTeclado);
 // NOTE: cuando se está definiendo el nombre de una funcion, una buena práctica es colocar letras mayusculas cuando hay nuevas palabras
-function eventoTeclado(objeto_evento){console.log(objeto_evento.keyCode);}
-//37
+function eventoTeclado(objeto_evento){
+  switch (objeto_evento.keyCode) {
+    case teclas.LEFT:
+      console.log("iz");
+      break;
+    case teclas.UP:
+      console.log("ar");
+      break;
+    case teclas.RIGHT:
+      console.log("dr");
+      break;
+    case teclas.DOWN:
+      console.log("ab");
+      break;
+    default:
+    console.log("otra");
+  }
+}
