0d59ec744ef7c9164429af1a40d87af28ca38fb5
Mauricio Martinez O <maodecolombia@gmail.com>
2018-09-29 13:57:00 -0500

    Definiendo canvas en js y generando funcion para "escuchar" el teclado

--- 005canvasconflechas/figura_teclado.js
+++ 005canvasconflechas/figura_teclado.js
@@ -1,1 +1,7 @@
-alert("hola carebola +");
+var canvas_js = document.getElementById("canvas_html");
+var lienzo = canvas_js.getContext("2D");
+ 
+document.addEventListener("keydown", eventoTeclado);
+// NOTE: cuando se está definiendo el nombre de una funcion, una buena práctica es colocar letras mayusculas cuando hay nuevas palabras
+function eventoTeclado(objeto_evento){console.log(objeto_evento.keyCode);}
+//37
