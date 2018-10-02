5c9359e3367db875e5e5d94dbae958cbb0a98290
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-02 10:38:22 -0500

    Función de números aleatorios dentro de un rango

--- 006JS_Math/VillaJS.js
+++ 006JS_Math/VillaJS.js
@@ -1,9 +1,16 @@
-document.write("la vaca hace 'mmmm'");
+document.write("la vaca hace 'mmmm' ");
 canvas_js=document.getElementById('canvas_html');
 lienzo=canvas_js.getContext('2d');
 
 
 
+function aleatorio(mini=10,maxim=20) {
+  z = Math.floor(Math.random()*((maxim-mini)+1))+mini
+  return z;
+}
+for (var i = 0; i < 10; i++) {
+  document.write (aleatorio(1,100) +", ")
+}
 
 
 lienzo.beginPath();
