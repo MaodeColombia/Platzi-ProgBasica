d7f8ee18c400f8674a75dd86865832ed75155c5c
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-09 18:21:39 -0500

    Buzz para multiplos de 5

--- 007FizzBuzz/FizzBuzz.js
+++ 007FizzBuzz/FizzBuzz.js
@@ -2,6 +2,9 @@ for (var i = 1; i <= 100; i++) {
   if (i%3==0) {
     document.write(i + "Fizz <br/>");
   }
+  else if (i%5 == 0) {
+    document.write(i + "Buzz <br/>");
+  }
   else {
     document.write(i + "<br/>")
   }
