a336b548968783b660ae40d200ae335cffde0806
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-09 18:46:14 -0500

    FizzBuzz Completo pero con modificación, función de módulo

--- 007FizzBuzz/FizzBuzz.js
+++ 007FizzBuzz/FizzBuzz.js
@@ -1,12 +1,15 @@
 for (var i = 1; i <= 100; i++) {
-  if (i%3 == 0) {
-    document.write("Fizz");
-  }
+  if (d(i,3)) {
+    document.write("Fizz");}
+
   if (i%5 == 0) {
-    document.write("Buzz");
-  }
-  if (i%3 != 0 && i%5 != 0) {
-    document.write(i);
-  }
-  document.write ("<br/>");
-}
+    document.write("Buzz");}
+
+  if (!d(i,3) && !d(i,5)) {
+    document.write(i);}
+
+  document.write ("<br/>");}
+
+function d(num,div) {
+  if (num % div == 0) {return true;}
+  else {return false;}}
