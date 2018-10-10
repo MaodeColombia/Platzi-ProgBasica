181504d5fc7c3099476269ce904d667694b2c0a3
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-09 20:51:56 -0500

    FUNCIONES de la clase

--- 008ClasesyArray/paki.js
+++ 008ClasesyArray/paki.js
@@ -2,10 +2,19 @@ class Pakiman
 {
   constructor()
   {
+    // NOTE: propiedades
     this.nombre = "vaca";
     this.tipo = "tierra"
     this.vida = 70;
     this.ataque = 7;
   }
+  // NOTE: funciones
+  hablar()
+  {
+    alert(this.nombre);
+  }
 }
+
+// NOTE: invocar PROPIEDAD "vaquera.ataque"
+// NOTE: invocar FUNCION "vaquera.hablar()"
 var vaquera = new Pakiman();
