8150c4f485b832ea805c1a48e52be7b9869cd970
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-09 21:46:05 -0500

    conceptos básicos de vestores

--- 008ClasesyArray/paki.js
+++ 008ClasesyArray/paki.js
@@ -21,3 +21,22 @@ var vaquera = new Pakiman();
 
 // NOTE: cambiar los valores de las PROPIEDADES por PARAMETROS del constructor
 var chancho = new Pakiman("cerdo",2,3,4);
+
+
+// NOTE: creacion de vector vacio
+var vector = [];
+
+// NOTE: agregando texto a la posición 0
+vector[0] = "hola";
+
+vector[1] = "Mao";
+
+vector[2] = 3.141592654;
+
+// NOTE: agrega la informacion en la posicion final del vector
+vector.push("otra");
+
+// NOTE: Array asociativo; es donde las posiciones están definida por un string
+// NOTE: ojo el mismo vector "vector" se asigna
+// NOTE: esto es lo que se llama un diccionario (estructura de datos de tipo diccionario)
+vector["Apellido"] = "Martín"
