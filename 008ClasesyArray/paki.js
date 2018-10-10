4c95e3c5dc9cdb82ff671190f591bc0225050087
Mauricio Martinez O <maodecolombia@gmail.com>
2018-10-09 20:44:07 -0500

    PROPIEDDADES de la clase

--- 008ClasesyArray/paki.js
+++ 008ClasesyArray/paki.js
@@ -1,1 +1,11 @@
-alert("s");
+class Pakiman
+{
+  constructor()
+  {
+    this.nombre = "vaca";
+    this.tipo = "tierra"
+    this.vida = 70;
+    this.ataque = 7;
+  }
+}
+var vaquera = new Pakiman();
